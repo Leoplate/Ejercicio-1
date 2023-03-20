3b445eab5a5d4a21469265ea534d44a7d94534d7
Leonardo Caceres <you@example.com>
2023-03-20 20:24:03 -0300

    creacion de api

--- src/app.js
+++ src/app.js
@@ -2,5 +2,22 @@ const vec = [1,2,3,4,5,6,7,8,9];
 const vec2 = []
 let conta = 0
 vec.filter( n => n % 2 == 0).map(n => conta = conta + n );
-console.log(conta);
-console.log("Hola Leo");
+//console.log(conta);
+//console.log("Hola Leo");
+
+
+//Importa una funcion
+const express = require('express')
+//Ejecuto la funcion y lo guardo en app
+const app = express();
+//puerto para el servidor
+const PORT = 3001;
+app.get('/api',(req, res)=> {
+    //res.json({mensaje:'hola mundo'}).status(200)
+    res.json({mensaje:'Ines Diaz Alegria'}).status(200)
+});
+
+
+
+//pongo la aplicacion a escuchar
+app.listen(PORT, () => {console.log('Estoy escuchando en el puerto:' + PORT)})
